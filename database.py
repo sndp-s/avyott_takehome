f5757e1ae53f0eae1bb446107f84f39b7de956e0
Sandeep Sharma <sandeeptech8@gmail.com>
2025-02-28 22:49:19 +0530

    feat(database): setup db connection pool and getter

---
+++ database.py
@@ -0,0 +1,45 @@
+"""
+Database utils
+"""
+
+import psycopg2
+from psycopg2.pool import SimpleConnectionPool
+from fastapi import HTTPException
+from config import settings
+
+db_pool = None
+
+
+def get_db_pool():
+    """
+    DB pool getter
+    """
+    global db_pool
+    if db_pool is None:
+        try:
+            db_pool = SimpleConnectionPool(
+                minconn=settings.minconn,
+                maxconn=settings.maxconn,
+                host=settings.db_host,
+                database=settings.db_name,
+                user=settings.db_user,
+                password=settings.db_password,
+                port=settings.db_port
+            )
+        except psycopg2.OperationalError as e:
+            # TODO :: log error here
+            raise HTTPException(
+                status_code=500, detail="Database connection error")
+    return db_pool
+
+
+def get_db():
+    """
+    DB connection getter
+    """
+    pool = get_db_pool()
+    conn = pool.getconn()
+    try:
+        yield conn
+    finally:
+        db_pool.putconn(conn)
