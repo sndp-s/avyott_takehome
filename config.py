5826aad22578687e9ed4fefbc9b8a221628eaedb
Sandeep Sharma <sandeeptech8@gmail.com>
2025-02-28 22:45:18 +0530

    feat(config): create app config

---
+++ config.py
@@ -0,0 +1,23 @@
+"""
+Application config
+"""
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """
+    Application Settings
+    """
+    model_config = SettingsConfigDict(env_file=".env")
+
+    app_name: str = "Library API"
+    db_host: str
+    db_name: str
+    db_user: str
+    db_password: str
+    db_port: str
+    minconn: int = 1
+    maxconn: int = 10
+
+settings = Settings()
