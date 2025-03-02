0b42e9b0d88d9e401eeddafeeea9fc949c702269
Sandeep Sharma <sandeeptech8@gmail.com>
2025-03-02 11:34:20 +0530

    feat(response): return a standard response from books apis

--- app/api/books.py
+++ app/api/books.py
@@ -7,6 +7,7 @@ from fastapi import APIRouter, Depends, Query, Path
 from app.db.connection import get_db
 from app.services import books as books_service
 from app.models import books as books_models
+from app.models.responses import APIResponse
 
 router = APIRouter()
 
@@ -17,36 +18,45 @@ async def get_all_books(
     # TODO :: Fix error on absence of optional query params
     offset: int = Annotated[int, Query(0, ge=0, description="Starting index")],
     limit: int = Annotated[int, Query(10, ge=1, le=100, description="Number of items to retrieve")]
-) -> list[books_models.BookResponse]:
+) -> APIResponse[list[books_models.Book]]:
     """
     Returns all the books in the library with their basic info.
     """
     all_books = books_service.get_all_books_service(db, {}, offset, limit)
-    return all_books
+    return APIResponse(
+        data=all_books,
+        message="Books fetched successfully"
+    )
 
 
 @router.get("/{book_id}")
 async def get_book(
     book_id: Annotated[int, Path()],
     db=Depends(get_db)
-) -> books_models.BookResponse:
+) -> APIResponse[books_models.Book]:
     """
     Returns the books corresponding to the given book_id
     """
     book = books_service.get_book_service(db, book_id)
-    return book
+    return APIResponse(
+        data=book,
+        message='Book fetched successfully'
+    )
 
 
 @router.post("")
 async def create_book(
     book: books_models.BookCreate,
     db=Depends(get_db)
-) -> int:
+) -> APIResponse[int]:
     """
     Adds a new book to the library.
     """
     added_book = books_service.add_new_book_service(db, book)
-    return added_book
+    return APIResponse(
+        data=added_book,
+        message="Book added successfully"
+    )
 
 
 @router.put("/{book_id}")
@@ -54,21 +64,27 @@ async def update_book(
     book_id: Annotated[int, Path()],
     book: books_models.BookUpdate,
     db=Depends(get_db)
-) -> int:
+) -> APIResponse[int]:
     """
     Update an existing book.
     """
     book_id = books_service.update_book_service(db, book_id, book.dict(exclude_unset=True))
-    return book_id
+    return APIResponse(
+        data=book_id,
+        message="Book updated successfully"
+    )
 
 
 @router.delete("/{book_id}")
 async def delete_book(
     book_id: Annotated[int, Path()],
     db=Depends(get_db)
-) -> bool:
+) -> APIResponse[bool]:
     """
     Deletes the book corresponsing to given book_id.
     """
     books_service.delete_book_service(db, book_id)
-    return True
+    return APIResponse(message="Book deleted")
+
+
+# TODO :: Implement books search API
